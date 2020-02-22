a5e08a4f40ea0924a3bd4bbf1f4d0b776ddad848
D. Zack Garza <dzackgarza@gmail.com>
2020-02-22 00:44:52 -0500

    Update

--- make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-filepath=CourseNotes.md
-
-cat "CourseNotes.md" | pandoc -f markdown --filter pandoc-include -r markdown+tex_math_dollars+simple_tables+table_captions+yaml_metadata_block+smart+blank_before_blockquote+backtick_code_blocks+link_attributes --template=template.tex --pdf-engine=pdflatex -o "CourseNotes.pdf" 
-
